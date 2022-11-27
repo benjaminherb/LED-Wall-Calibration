8fc3936a149be9254d306dd5d5b244fc4e330e02
pingu@benjamin-herb.de <pingu@benjamin-herb.de>
2022-11-27 12:52:02 +0100

    reworked eval script

--- scripts/evaluate_response_curve.m
+++ scripts/evaluate_response_curve.m
@@ -1,82 +1,79 @@
-%% USER CONFIG
+%% SETUP
+
+clear
+
+% USER CONFIG
 
 conf.data_dir ="../measurements/response_curve/";
 conf.gamma = 2.2;
+conf.bit_depth= 8;
 
-%% PREREQUISITES
+
+% PREPARATION
+
+conf.value_length = 2 ^ conf.bit_depth;
+conf.max_value = conf.value_length - 1;
+
+
+% LOAD DATA
 
 files = dir(conf.data_dir + "/*.json")';
-response_curves = NaN(256, length(files));
+response_curves_absolute = NaN(length(files), conf.value_length);
 for i = 1:1:length(files)
     json_text = fileread(string(files(i).folder) + "/" + string(files(i).name));
     data = jsondecode(json_text);
     
     for j = 1:1:length(data')
-        response_curves(j,i) = data(j).Yxy.Y;
+        if (data(j).Yxy.value == "-0008") % in accurate readings
+            response_curves_absolute(i,j) = 0;
+        else
+            response_curves_absolute(i,j) = data(j).Yxy.Y;
+        end
     end
 end
+
+values_absolute = 0:1:conf.max_value;
+values_scaled = values_absolute / conf.max_value;
+reference_curve = values_scaled .^ conf.gamma;
+response_curves_scaled = NaN(size(response_curves_absolute));
+for i = 1:height(response_curves_absolute)
+    response_curves_scaled(i,:) = response_curves_absolute(i,:) ./ response_curves_absolute(i, end);
+end
+
+clear("rc", "i", "j", "json_text", "files"); % clear temp variables
 
 %% PLOT
-
-values = 0:1:255;
-scaled_values = values ./255;
-reference_curve = scaled_values .^ conf.gamma;
-
-figure();
-plot(values, reference_curve, 'cyan');
-hold on
-plot(values, response_curves(:,1) ./ response_curves(end, 1), 'black');
-plot(values, response_curves(:,2) ./ response_curves(end, 2), 'red');
-plot(values, response_curves(:,3) ./ response_curves(end, 3), 'green');
-plot(values, response_curves(:,4) ./ response_curves(end, 4), 'blue');
-hold off
-legend('reference','white','red','green','blue');
-%%
+figure("Name", "Response Curve Comparison", "NumberTitle", "off", "Position", [0 0 1300 600] );
+tiledlayout(1,2);
 
 
+% FULL PLOT
+full_range = nexttile;
 
+plot(values_absolute, reference_curve, 'black:' ...
+, values_absolute, response_curves_scaled(1,:), 'black' ...
+, values_absolute, response_curves_scaled(2,:), 'red' ...
+, values_absolute, response_curves_scaled(3,:), 'green' ...
+, values_absolute, response_curves_scaled(4,:), 'blue' ...
+);
 
+legend('gamma 2.2','white','red','green','blue');
+xlabel('Input Code Value [0-255]');
+ylabel('Measured Luminance Output [0-1]');
+xlim([0 255]);
 
-fig_abs = figure('Name', 'Gamma Curve compared to measured response curve');
+% DETAIL
+detail_range = nexttile;
 
-% MEASURED RESPONSE CURVE (Scaled Y Values)
-plot(data.Value, measured_curve, 'black'); % labels used for color
-hold on
-% REFERENCE GAMMA CURVE
-plot(data.Value, reference_curve, 'red');
-plot(data.Value, reference_26, 'green');
+plot(values_absolute, reference_curve, 'black:' ...
+, values_absolute, response_curves_scaled(1,:), 'black' ...
+, values_absolute, response_curves_scaled(2,:), 'red' ...
+, values_absolute, response_curves_scaled(3,:), 'green' ...
+, values_absolute, response_curves_scaled(4,:), 'blue' ...
+);
 
-
-%% DIFFERENCE
-
-fig_dev = figure('Name', 'Measured deviation from reference gamma');
-plot(values, reference_curve - measured_curve);
-
-%% GENERATE SIMPLE OFFSET LUT
-
-
-if not(isfolder(OUTPUT_DIR))
-    mkdir(OUTPUT_DIR)
-end
-
-TS_STR = datestr(datetime,'yyyymmdd_HHMMss_');
-
-GAMMA_STR = string(GAMMA*10); % used for filenames/start of gamdat file
-
-offset_curve = reference_curve + (reference_curve - measured_curve);
-offset_curve = offset_curve .* 2^16;
-offset_curve = uint16(offset_curve);
-
-output_file = fopen(OUTPUT_DIR + TS_STR + FILE_NAME + ".gamdat", 'w');
-fprintf(output_file, GAMMA_STR + '*0#255#0#65535#');
-
-first_number = 1; % Avoid the comma at the eof
-for i = 1:1:length(offset_curve)
-    if(first_number)
-        fprintf(output_file, string(offset_curve(i)));
-        first_number = 0;
-    else
-        fprintf(output_file, "," + string(offset_curve(i)));
-    end
-end
-
+legend('gamma 2.2','white','red','green','blue');
+xlabel('Input Code Value [0-255]');
+ylabel('Measured Luminance Output [0-1]');
+xlim([240 255]);
+ylim([0.95 1.05]);
