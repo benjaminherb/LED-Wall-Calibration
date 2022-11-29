732d24b38026b1e6fe62cdc0c77a0c7d665ab3be
Benjamin Herb <pingu@benjamin-herb.de>
2022-11-29 11:40:47 +0100

    Added rgb split from grey curve as comparison

--- scripts/evaluate_response_curve.m
+++ scripts/evaluate_response_curve.m
@@ -7,6 +7,7 @@ clear
 conf.data_dir ="../measurements/response_curve/";
 conf.gamma = 2.2;
 conf.bit_depth= 8;
+conf.intermediate_colorspace = "linear-rgb"; % eg. "srgb" (default) | "adobe-rgb-1998" | "prophoto-rgb" | "linear-rgb"
 
 
 % PREPARATION
@@ -39,36 +40,70 @@ for i = 1:height(response_curves_absolute)
     response_curves_scaled(i,:) = response_curves_absolute(i,:) ./ response_curves_absolute(i, end);
 end
 
-clear("rc", "i", "j", "json_text", "files", "data"); % clear temp variables
+% SPLIT GREY CURVE TO RGB
+response_curves_rgb_split = NaN(4, conf.value_length);
+json_text = fileread(string(files(1).folder) + "/" + string(files(1).name));
+data = jsondecode(json_text);
+ref_lum = data(end).Yxy.Y;
+
+for j = 1:1:length(data')
+    if (data(j).Yxy.value == "-0008") % in accurate readings
+        response_curves_absolute(:,j) = 0;
+    else
+        rgb = xyz2rgb([...
+            data(j).XYZ.X / ref_lum, ...
+            data(j).XYZ.Y / ref_lum, ...
+            data(j).XYZ.Z / ref_lum], ...
+            "ColorSpace", conf.intermediate_colorspace);
+        
+        grey = data(j).XYZ.Y / ref_lum;
+        red = rgb2xyz([rgb(1), 0, 0], "ColorSpace", conf.intermediate_colorspace);
+        green = rgb2xyz([0, rgb(2), 0], "ColorSpace", conf.intermediate_colorspace);
+        blue =rgb2xyz([0, 0, rgb(3)], "ColorSpace", conf.intermediate_colorspace);
+        
+        response_curves_rgb_split(:,j) = [grey, red(2), green(2), blue(2)];
+    end
+end
+
+for i = 1:height(response_curves_rgb_split)
+    response_curves_rgb_split(i,:) = response_curves_rgb_split(i,:) ./ response_curves_rgb_split(i, end);
+end
+
+clear("rc", "i", "j", "json_text", "files", "data", "grey", "red", "green", "blue", "rgb"); % clear temp variables
 
 %% PLOT
-figure("Name", "Response Curve Comparison", "NumberTitle", "off", "Position", [0 0 1300 600] );
-tiledlayout(1,2);
+figure("Name", "Response Curve Comparison", "NumberTitle", "off", "Position", [0 0 1000 1000] );
+tiledlayout(2,2);
+
+plot_full_and_detail(values_absolute, response_curves_scaled, reference_curve, 'RGB Channels Measured Separately');
+plot_full_and_detail(values_absolute, response_curves_rgb_split, reference_curve, 'RGB Channels From Grey Curve');
 
 
+%% HELPER FUNCTIONS
+function plot_full_and_detail(values, response_curves, reference_curve, name)
 % FULL PLOT
-full_range = nexttile;
-
-plot(values_absolute, reference_curve, 'black:' ...
-    , values_absolute, response_curves_scaled(1,:), 'black' ...
-    , values_absolute, response_curves_scaled(2,:), 'red' ...
-    , values_absolute, response_curves_scaled(3,:), 'green' ...
-    , values_absolute, response_curves_scaled(4,:), 'blue' ...
+first = nexttile;
+plot(values, reference_curve, 'black:' ...
+    , values, response_curves(1,:), 'black' ...
+    , values, response_curves(2,:), 'red' ...
+    , values, response_curves(3,:), 'green' ...
+    , values, response_curves(4,:), 'blue' ...
     );
 
 legend('gamma 2.2','white','red','green','blue', 'location', 'northwest');
 xlabel('Input Code Value [0-255]');
 ylabel('Measured Luminance Output [0-1]');
 xlim([0 255]);
+title(first, name);
+
 
 % DETAIL
-detail_range = nexttile;
-
-plot(values_absolute, reference_curve, 'black:' ...
-    , values_absolute, response_curves_scaled(1,:), 'black' ...
-    , values_absolute, response_curves_scaled(2,:), 'red' ...
-    , values_absolute, response_curves_scaled(3,:), 'green' ...
-    , values_absolute, response_curves_scaled(4,:), 'blue' ...
+second = nexttile;
+plot(values, reference_curve, 'black:' ...
+    , values, response_curves(1,:), 'black' ...
+    , values, response_curves(2,:), 'red' ...
+    , values, response_curves(3,:), 'green' ...
+    , values, response_curves(4,:), 'blue' ...
     );
 
 legend('gamma 2.2','white','red','green','blue', 'location', 'northwest');
@@ -76,3 +111,5 @@ xlabel('Input Code Value [0-255]');
 ylabel('Measured Luminance Output [0-1]');
 xlim([235 255]);
 ylim([0.90 1.01]);
+title(second, name);
+end
