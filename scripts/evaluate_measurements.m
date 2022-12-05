6504c001cf23079ea0d1a52db56568bcbb44f7e8
Benjamin Herb <pingu@benjamin-herb.de>
2022-12-05 17:58:25 +0100

    updated measurement evaluation

--- scripts/evaluate_measurements.m
+++ scripts/evaluate_measurements.m
@@ -17,7 +17,23 @@ plot_Lab(data)
 nexttile()
 plot_Luv(data)
 
- 
+function plot_space_CIELAB(color_space)
+% SRGB
+% Xw = 0.950;
+% Yw = 1.000;
+% Zw = 1.089;
+
+values = get_all_values(); % ALL the values
+
+values_lab = NaN(length(values), 6);
+for i = 1:1:length(values)
+    values_lab(i,1:3) = rgb2lab(values(i,:), "ColorSpace", color_space);
+    values_lab(i,4:6) = values(i,:);
+end
+
+scatter3(values_lab(:,2), values_lab(:,3), values_lab(:, 1), 50, values_lab(:,4:6), '.');
+
+end
 function plot_Luv(data)
 reference_white = data.grey(end).XYZ;
 
@@ -48,10 +64,10 @@ function r = get_Luv(data, refWP)
 r = NaN(length(data), 6);
 
 for i = 1:1:256
- 
+    
     % GET L*u*v*
     [L, u, v] = XYZ_to_CIELUV(data(i).XYZ.X, data(i).XYZ.Y, data(i).XYZ.Z, ...
-                              refWP.X,refWP.Z,refWP.Z);
+        refWP.X,refWP.Z,refWP.Z);
     
     %u = data(i).Yuv.u; % u' w/o WP compensation
     %v = data(i).Yuv.v; % v' w/o WP compensation
@@ -79,10 +95,12 @@ gLab = get_Lab(data.green, reference_white);
 bLab = get_Lab(data.blue, reference_white);
 
 hold on
-scatter3(wLab(:,2), wLab(:,3), wLab(:,1), 50, wLab(:,4:6)./255);
-scatter3(rLab(:,2), rLab(:,3), rLab(:,1), 50, rLab(:,4:6)./255);
-scatter3(gLab(:,2), gLab(:,3), gLab(:,1), 50, gLab(:,4:6)./255);
-scatter3(bLab(:,2), bLab(:,3), bLab(:,1), 50, bLab(:,4:6)./255);
+scatter3(wLab(:,2), wLab(:,3), wLab(:,1), 50, wLab(:,4:6)./255, 'filled');
+scatter3(rLab(:,2), rLab(:,3), rLab(:,1), 50, rLab(:,4:6)./255, 'filled');
+scatter3(gLab(:,2), gLab(:,3), gLab(:,1), 50, gLab(:,4:6)./255, 'filled');
+scatter3(bLab(:,2), bLab(:,3), bLab(:,1), 50, bLab(:,4:6)./255, 'filled');
+plot_space_CIELAB("srgb") 
+
 xlabel("a*");
 ylabel("b*");
 zlabel("L*");
@@ -108,11 +126,44 @@ for i = 1:1:256
         name = name';
     end
     
-    [L, a, b] = XYZ_to_CIELAB(X, Y, Z, refWP.X, refWP.Y, refWP.Z); 
+    [L, a, b] = XYZ_to_CIELAB(X, Y, Z, refWP.X, refWP.Y, refWP.Z);
     
     if data(i).Yxy.value == "-0008"
         L=0; a=0; b=0; name=[0,0,0];
     end
     r(i,:) = [L, a, b, name];
 end
+end
+
+function values = get_black_to_white_test_values()
+values = NaN(1,3);
+for i=0:1:255; values = [values; [  i,  0,  0]]; end
+for i=0:1:255; values = [values; [  i,255,255]]; end
+for i=0:1:255; values = [values; [255,  i,  i]]; end
+for i=0:1:255; values = [values; [  0,  i,  i]]; end
+for i=0:1:255; values = [values; [  0,  i,  0]]; end
+for i=0:1:255; values = [values; [255,  i,255]]; end
+for i=0:1:255; values = [values; [  i,255,  i]]; end
+for i=0:1:255; values = [values; [  i,  0,  i]]; end
+for i=0:1:255; values = [values; [  0,  0,  i]]; end
+for i=0:1:255; values = [values; [255,255,  i]]; end
+for i=0:1:255; values = [values; [  i,  i,255]]; end
+for i=0:1:255; values = [values; [  i,  i,  0]]; end
+for i=0:1:255; values = [values; [  i,  i,  i]]; end
+end
+
+function values = get_all_values()
+steps = 32;
+values = NaN(steps^3,3);
+i = 1;
+for r = 0:1/steps:1
+    disp(i)
+    for g = 0:1/steps:1
+        for b = 0:1/steps:1
+            values(i,: ) = [r, g, b];
+            i = i+1;
+        end
+    end
+end
+
 end
