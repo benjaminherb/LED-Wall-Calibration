704001053d9460e5f9ac12a607279965a184fbcd
Kumpelsift <michael.t.beil@web.de>
2022-12-19 00:28:18 +0100

    sRGb und PQ implementiert

--- scripts/generate_lut_and_gradient.m
+++ scripts/generate_lut_and_gradient.m
@@ -4,8 +4,11 @@
 % Test Wall: 384x256
 % LED Wall:  768x768 (x2)
 
+addpath("..\utils\");
+
 %% INPUT
 
+transfer_curve = "pq";      % pq, sRGB oder gamma
 GAMMA = 2.2;
 OUTPUT_DIR = "../output";
 
@@ -15,7 +18,7 @@ HEIGHT = 1080;
 % Pad is used if the gradient needs to be included in a bigger frame
 % (mostly for the test panel)
 % set to the same as the WIDTH/HEIGHT if not needed
-PAD_WIDTH = 1920; 
+PAD_WIDTH = 1920;
 PAD_HEIGHT = 1080;
 
 STEP_WIDTHS = [1,2, 4, 8, 16, 32, 64, 128];
@@ -36,19 +39,37 @@ if not(isfolder(GRAD_DIR))
     mkdir(GRAD_DIR)
 end
 
-%% BASE VECTOR 
+%% BASE VECTOR
 
 eight_bit_lin = (0:1:255)./255;
-eight_bit_gamma = eight_bit_lin .^ GAMMA;
+eight_bit_curve = NaN(size(eight_bit_lin));
 
+switch(transfer_curve)
+    case "pq"
+        for i = 1:1:length(eight_bit_lin)
+            eight_bit_curve(i) = linear_to_PQ(eight_bit_lin(i));
+        end
+        fileprefix = "LUT_PQ_";
+    case "srgb"
+        for i = 1:1:length(eight_bit_lin)
+            eight_bit_curve(i) = linear_to_sRGB(eight_bit_lin(i));
+        end
+        fileprefix = "LUT_sRGB_";
+    case "gamma"
+        eight_bit_curve = eight_bit_lin .^ GAMMA;
+        fileprefix = "LUT_GAMMA_" + GAMMA_STR + "_";
+    otherwise
+        error("Unkknown transfercurve: " + transfer_curve);
+end
 %% GENERATE LUT FILES
 
 for n = 1:15
-    scaled_vector = (eight_bit_gamma .* 2^n) + 2^n;
+    scaled_vector = (eight_bit_curve .* 2^n) + 2^n;
     scaled_vector = uint16(scaled_vector);
-    output_file = fopen(LUT_DIR + "LUT_" + "GAMMA_" + GAMMA_STR + "_" + scaled_vector(1) + "_" + scaled_vector(256)  +  ".gamdat", 'w');
+
+    output_file = fopen(LUT_DIR + fileprefix + scaled_vector(1) + "_" + scaled_vector(256)  +  ".gamdat", 'w');
     fprintf(output_file, GAMMA_STR + '*0#255#0#65535#');
-    
+
     first_number = 1; % Avoid the comma at the eof
     for v = scaled_vector
         if(first_number)
@@ -77,7 +98,7 @@ for step_width = STEP_WIDTHS
     end
     output_image = GRAD_DIR + "GRADIENT_"  + WIDTH + "_" + HEIGHT + "_" + step_width + ".png";
     img = padarray(img, [PAD_HEIGHT, PAD_WIDTH], 0, 'post');
-    imwrite(img, output_image);   
+    imwrite(img, output_image);
 end
 
 
