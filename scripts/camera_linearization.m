749afb799ce6aa44969dde737a0153c9f9265851
pingu@benjamin-herb.de <pingu@benjamin-herb.de>
2023-02-01 19:48:41 +0100

    refactored script and fixed bug when applying the matrix

--- scripts/camera_linearization.m
+++ scripts/camera_linearization.m
@@ -1,97 +1,142 @@
+close all;
+clear;
+clc;
 
 addpath("../utils/");
 
-TEST_IMG = imread("../data/testchart_blur2.JPG");
-ORIGINAL_IMG =  imread("../output/PRIMARIES/PRIMARIES_CAMERA_TEST_384_256.png");
-%%
+captured_testpattern = imread("../data/testchart_blur2.JPG");
+original_testpattern = imread("../output/PRIMARIES/PRIMARIES_CAMERA_TEST_384_256.png");
 
-TEST_IMG = TEST_IMG(1:2000, 1:3000, :);
-[HEIGHT,WIDTH, ~] = size(TEST_IMG);
-PATCH_RADIUS = 200;
+%% Get values and calculate matrix
 
-red_patch = TEST_IMG(HEIGHT/4-PATCH_RADIUS:HEIGHT/4+PATCH_RADIUS, WIDTH/6-PATCH_RADIUS:WIDTH/6+PATCH_RADIUS, :);
-green_patch = TEST_IMG(HEIGHT/4-PATCH_RADIUS:HEIGHT/4+PATCH_RADIUS, (WIDTH/6)*3-PATCH_RADIUS:(WIDTH/6)*3+PATCH_RADIUS, :);
-blue_patch = TEST_IMG(HEIGHT/4-PATCH_RADIUS:HEIGHT/4+PATCH_RADIUS,(WIDTH/6)*5-PATCH_RADIUS:(WIDTH/6)*5+PATCH_RADIUS, :);
+captured_testpattern = im2double(captured_testpattern);
 
+patch_captured.red   = get_patch_from_test_image(captured_testpattern,1,1);
+patch_captured.green = get_patch_from_test_image(captured_testpattern,2,1);
+patch_captured.blue  = get_patch_from_test_image(captured_testpattern,3,1);
+patch_captured.black = get_patch_from_test_image(captured_testpattern,1,2);
+patch_captured.grey  = get_patch_from_test_image(captured_testpattern,2,2);
+patch_captured.white = get_patch_from_test_image(captured_testpattern,3,2);
 
-black_patch = TEST_IMG((HEIGHT/4)*3-PATCH_RADIUS:(HEIGHT/4)*3+PATCH_RADIUS, WIDTH/6-PATCH_RADIUS:WIDTH/6+PATCH_RADIUS, :);
-grey_patch = TEST_IMG((HEIGHT/4)*3-PATCH_RADIUS:(HEIGHT/4)*3+PATCH_RADIUS, (WIDTH/6)*3-PATCH_RADIUS:(WIDTH/6)*3+PATCH_RADIUS, :);
-white_patch = TEST_IMG((HEIGHT/4)*3-PATCH_RADIUS:(HEIGHT/4)*3+PATCH_RADIUS,(WIDTH/6)*5-PATCH_RADIUS:(WIDTH/6)*5+PATCH_RADIUS, :);
+red_captured   = get_lin_mean_rgb_from_patch(patch_captured.red);
+green_captured = get_lin_mean_rgb_from_patch(patch_captured.green);
+blue_captured  = get_lin_mean_rgb_from_patch(patch_captured.blue);
+white_captured = get_lin_mean_rgb_from_patch(patch_captured.white);
 
+calibration_matrix = calculate_calibration_matrix( ...
+    red_captured, green_captured, blue_captured, white_captured);
 
+disp("Calibration Matrix");
+disp(calibration_matrix);
 
-tiledlayout(2,3);
+%% Apply matrix to the image
 
-nexttile()
-imshow(red_patch);
-nexttile()
-imshow(green_patch);
-nexttile()
-imshow(blue_patch);
-nexttile()
-imshow(black_patch);
-nexttile()
-imshow(grey_patch);
-nexttile()
-imshow(white_patch);
+corrected_testpattern = zeros(size(captured_testpattern));
 
-%%
-
-rgb_red_patch = get_lin_rgb_from_patch(red_patch);
-rgb_green_patch = get_lin_rgb_from_patch(green_patch);
-rgb_blue_patch = get_lin_rgb_from_patch(blue_patch);
-rgb_white_patch = get_lin_rgb_from_patch(white_patch);
-
-%%
-
-rgb_f = [rgb_red_patch; rgb_green_patch; rgb_blue_patch]
-
-rgb_white_scaled = rgb_white_patch./max(rgb_white_patch)
-
-S = rgb_f^(-1)*rgb_white_scaled'
-
-rgb_f = rgb_f * [S(1), 0, 0; 0, S(2), 0; 0, 0, S(3)]
-
-calibration_matrix = rgb_f^(-1)
-
-%%
-
-corrected_img = zeros(size(TEST_IMG));
-
-for x=1:height(TEST_IMG)
-    for y=1:width(TEST_IMG)
-        rgb = [sRGB_to_linear(double(TEST_IMG(x,y,1))./255), sRGB_to_linear(double(TEST_IMG(x,y,2))./255), sRGB_to_linear(double(TEST_IMG(x,y,3))./255)];
-        rgb = rgb'./calibration_matrix;
-        rgb_flipped = cat(3,linear_to_sRGB(rgb(1)), linear_to_sRGB(rgb(2)), linear_to_sRGB(rgb(3)));
-        corrected_img(x,y,:) = rgb_flipped;
+for y=1:height(captured_testpattern)
+    for x=1:width(captured_testpattern)
+        
+        rgb = [sRGB_to_linear(captured_testpattern(y,x,1)), ...
+            sRGB_to_linear(captured_testpattern(y,x,2)), ...
+            sRGB_to_linear(captured_testpattern(y,x,3))];
+        
+        rgb = rgb * calibration_matrix;
+        rgb = clip_values(rgb);
+        
+        corrected_testpattern(y,x,:) = cat(3, linear_to_sRGB(rgb(1)), ...
+            linear_to_sRGB(rgb(2)), ...
+            linear_to_sRGB(rgb(3)));
     end
 end
 
+patch_corrected.red   = get_patch_from_test_image(corrected_testpattern,1,1);
+patch_corrected.green = get_patch_from_test_image(corrected_testpattern,2,1);
+patch_corrected.blue  = get_patch_from_test_image(corrected_testpattern,3,1);
+patch_corrected.black = get_patch_from_test_image(corrected_testpattern,1,2);
+patch_corrected.grey  = get_patch_from_test_image(corrected_testpattern,2,2);
+patch_corrected.white = get_patch_from_test_image(corrected_testpattern,3,2);
 
-imshow(corrected_img);
+%% Compare results
+
+compare_captured_corrected(patch_captured, patch_corrected , 3)
+
+figure();
+tiledlayout(2,1);
+nexttile();
+imshow(captured_testpattern);
+nexttile();
+imshow(corrected_testpattern);
+
+function compare_captured_corrected(patch_captured, patch_corrected, comparison_amount)
+
+patch_names = fieldnames(patch_captured);
+
+if (~exist("comparison_amount", "var") || comparison_amount > length(patch_names))
+    comparison_amount = length(patch_names);
+end
+
+figure();
+tiledlayout(comparison_amount,2);
+
+for i = 1:comparison_amount
+    disp("Captured " + patch_names{i});
+    disp(get_lin_mean_rgb_from_patch(patch_captured.(patch_names{i})));
+    disp("Corrected " + patch_names{i});
+    disp(get_lin_mean_rgb_from_patch(patch_corrected.(patch_names{i})));
+
+    nexttile();
+    imshow(patch_captured.(patch_names{i}));
+    nexttile();
+    imshow(patch_corrected.(patch_names{i}));
+end
+
+end
 
 
-
-%%
-
-function rgb = get_lin_rgb_from_patch(img)
-
+function rgb = get_lin_mean_rgb_from_patch(img)
 lin_rgb = NaN(size(img));
-
 img = double(img);
 
 for x=1:length(img)
     for y=1:length(img)
         for c=1:3
-            lin_rgb(x,y,c) =sRGB_to_linear(img(x,y,c)./255);
+            lin_rgb(x,y,c) =sRGB_to_linear(img(x,y,c));
         end
     end
 end
-
 rgb = [mean(lin_rgb(:,:,1), 'all'), mean(lin_rgb(:,:,2), 'all'), mean(lin_rgb(:,:,3), 'all')];
+end
 
-% XYZ= RGB_to_XYZ(rgb(1), rgb(2), rgb(3), "srgb", "D65", "srgb");
+function patch = get_patch_from_test_image(img, x, y, radius)
 
+% normalize HxW to be devisable by 2 and 3
+[height,width, ~] = size(img);
+height = height - mod(height,2);
+width = width - mod(width,3);
+img = img(1:height,1:width,:);
 
+if ~exist("radius", "var")
+    radius = round(height/10);
+end
 
+patch = img( (height/4) * (2*y-1) - radius : (height/4) * (2*y-1) + radius, ...
+             ( width/6) * (2*x-1) - radius : ( width/6) * (2*x-1) + radius, :);
+
+end
+
+function matrix = calculate_calibration_matrix(red, green, blue, white)
+f = [red; green; blue];
+white_scaled = white ./ max(white);
+S = f^(-1) * white_scaled';
+f = f * [S(1), 0, 0; ...
+         0, S(2), 0; ...
+         0, 0, S(3)];
+matrix = f^(-1);
+end
+
+function rgb = clip_values(rgb)
+% clamp values between 0 and 1
+rgb = [max(min(1,rgb(1)),0), ...
+       max(min(1,rgb(2)),0), ...
+       max(min(1,rgb(3)),0)];
 end
