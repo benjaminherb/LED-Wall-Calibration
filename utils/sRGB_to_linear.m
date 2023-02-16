84ff901c23720acf3c4a7730d5dd50a6c0c55b62
pingu@benjamin-herb.de <pingu@benjamin-herb.de>
2023-02-16 23:08:52 +0100

    Allow for vectorized operations

--- utils/sRGB_to_linear.m
+++ utils/sRGB_to_linear.m
@@ -1,5 +1,5 @@
 function y = sRGB_to_linear(x)
 % SRGB_TO_LINEAR Converts value from sRGB to linear
-y = (x>0.04045)*((x+0.055)/1.055)^2.4 + (x<=0.04045)*(x/12.92);
+y = (x>0.04045).*((x+0.055)./1.055).^2.4 + (x<=0.04045).*(x./12.92);
 end
 
